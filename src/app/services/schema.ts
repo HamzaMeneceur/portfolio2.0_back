0b6cbe6fd4dca28878e4eec2f0fc0a4a3211d819
Hamza Meneceur <hamza.meneceur.dev@gmail.com>
2024-06-12 10:18:33 +0200

    hmz - schema user regex ok

--- src/app/services/schema.ts
+++ src/app/services/schema.ts
@@ -1,3 +1,12 @@
 import Joi from "joi";
-
+const emailRegExp = new RegExp('^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
+const passwordRegExp = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$');
+const schema = Joi.object({
+    email: Joi.string().email().pattern(emailRegExp).required(),
+    password: Joi.string().pattern(passwordRegExp).required()
+})
 // interface user
+interface user {
+    email: string,
+    password: string
+}
