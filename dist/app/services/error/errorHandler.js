b02646d146a4d1f1ed930feae7caf8d78f4529fe
Hamza Meneceur <hamza.meneceur.dev@gmail.com>
2024-06-13 14:33:15 +0200

    maj

---
+++ dist/app/services/error/errorHandler.js
@@ -0,0 +1,9 @@
+"use strict";
+/**
+ *
+ * utilisation de debug ?
+ * fs pour la sauvegarde en dur
+ * ! ne pas oublié d'ajouter au gitignore !!
+ *  utilisation de bunyan ou winston ?
+ */
+// switch case des code d'état http 
